b45108fc2d44082743d8b7dbec2a23cac46b53f6
Marialvo junior <marialvo.cfj@puccampinas.edu.br>
2026-04-04 20:43:52 -0300

    Implementa módulo de votação com submenus e controle de navegação
    "Implementado módulo de votação com navegação completa, incluindo submenus de votação, auditoria e resultados. Ajustado fluxo de votação para permitir saída apenas após encerramento. Atualizado menu principal com opção de encerramento do sistema."

--- main.py
+++ main.py
@@ -83,4 +83,77 @@ while (op_mod != 0):
                         msg.alerta("Voltando à seleção dos módulos...")
                     case _:
                         msg.erro("Opção inválida.")
+
+        case 2:
+            op_vot = -1
+
+            while (op_vot != 0):
+                print(menu.vot_menu)
+                op_vot = menu.selecionar_opcao()
+
+                match op_vot:
+                    
+                    #=================== ABRIR VOTAÇÃO ====================
+                    case 1:
+                        op_votacao = 0
+
+                        while (op_votacao != 2):
+                            print(cor.ciano("\n█▓▒▒░░░ ABRIR SISTEMA DE VOTAÇÃO ░░░▒▒▓█"))
+                            print(menu.vot_menu_votacao)
+                            op_votacao = menu.selecionar_opcao()
+
+                            match op_votacao:
+                                case 1:
+                                    msg.alerta("[Votar]")
+                                case 2:
+                                    msg.alerta("[Encerrar votação]")
+                                case _:
+                                    msg.erro("Opção inválida.")
+
+                    #=================== AUDITORIA ====================
+                    case 2:
+                        op_auditoria = -1
+
+                        while (op_auditoria != 0):
+                            print(cor.ciano("\n█▓▒▒░░░ AUDITORIA DO SISTEMA DE VOTAÇÃO ░░░▒▒▓█"))
+                            print(menu.vot_menu_auditoria)
+                            op_auditoria = menu.selecionar_opcao()
+
+                            match op_auditoria:
+                                case 1:
+                                    msg.alerta("[Exibir Logs de Ocorrências]")
+                                case 2:
+                                    msg.alerta("[Exibir Protocolos da Votação]")
+                                case 0:
+                                    msg.alerta("Voltando para o menu anterior...")
+                                case _:
+                                    msg.erro("Opção inválida.")
+
+                    #=================== RESULTADO ====================
+                    case 3:
+                        op_resultado = -1
+
+                        while (op_resultado != 0):
+                            print(cor.ciano("\n█▓▒▒░░░ RESULTADO DA VOTAÇÃO ░░░▒▒▓█"))
+                            print(menu.vot_menu_resultado)
+                            op_resultado = menu.selecionar_opcao()
+
+                            match op_resultado:
+                                case 1:
+                                    msg.alerta("[Boletim de Urna]")
+                                case 2:
+                                    msg.alerta("[Estatística de Comparecimento]")
+                                case 3:
+                                    msg.alerta("[Votos por Partido]")
+                                case 4:
+                                    msg.alerta("[Validação da Integridade dos Votos]")
+                                case 0:
+                                    msg.alerta("Voltando para o menu anterior...")
+                                case _:
+                                    msg.erro("Opção inválida.")
+                        
+                    case 0:
+                        msg.alerta("Voltando à seleção dos módulos...")
+                    case _:
+                        msg.erro("Opção inválida.")
                         
