e8331ce1bc98232b12acb627703c495fcc47e72e
MarialvoJrPUCC <marialvo.cfj@puccampinas.edu.br>
2026-04-10 08:57:11 -0300

    Add candidates and votes tables to schema

--- banco/schema.sql
+++ banco/schema.sql
@@ -8,3 +8,16 @@ CREATE TABLE IF NOT EXISTS eleitores (
     is_mesario BOOLEAN NOT NULL DEFAULT FALSE,
     status_voto BOOLEAN DEFAULT FALSE
 );
+-- Tabelas "candidatos" e "votos" (Marialvo):
+CREATE TABLE IF NOT EXISTS candidatos(
+id_candidato INT PRIMARY KEY AUTO_INCREMENT,
+nome_candidato VARCHAR(100) NOT NULL,
+partido_candidato VARCHAR(50) NOT NULL,
+numero_candidato INT NOT NULL UNIQUE);
+
+CREATE TABLE IF NOT EXISTS votos(
+id_voto INT PRIMARY KEY AUTO_INCREMENT,
+id_candidato INT NOT NULL,
+protocolo VARCHAR(100) NOT NULL UNIQUE,
+data_hora DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+FOREIGN KEY (id_candidato) REFERENCES candidatos(id_candidato));
